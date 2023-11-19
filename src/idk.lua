ebca77735543cd40551cd650cc88b4495388466a
1Vu5dK42kyjt1cCm <138992441+1Vu5dK42kyjt1cCm@users.noreply.github.com>
2023-11-19 18:37:56 +0800

    k

--- src/idk.lua
+++ src/idk.lua
@@ -1,5 +1,5 @@
 key = true
 while key  do
     game:GetService("ReplicatedStorage").Events.e8eGb8RgRXFcug8q:FireServer(workspace.HitParts.Snow1, "Snow6", "MagicWand")
-    os.wait(0.1)
+    task.wait(0.1)
 end
